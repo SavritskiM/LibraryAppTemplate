7a21b83547dc556eb24540e3c68574bbf7b42247
LLiu6 <36411010+LLiu6@users.noreply.github.com>
2018-03-23 08:56:41 -0400

    Update Library.java

--- src/Library.java
+++ src/Library.java
@@ -2,41 +2,41 @@
  * Library class
  */
 public class Library {
-    String name;
+	String name;
 	Address location;
 	Book[]books;
 	Member[]members;
 
-    /**
-     * addBook(Book) : void
-     * Creates a new array which is a copy of this.books, but, whose length is
-     * this.books.length + 1, to be able to add the new book.
-     *
-     * this.books is then replaced by the new array.
-     *
-     * @param book
-     *          A new book object to add to the books list
-     */
-     public void addBook(Book e){
-	    Book[]newBook=Book[books.length+1];
-	    for(int i=0;i<books.length;i++){
-	    	newBook[i]=books[i];
-	    }
-	    newBook[books.length]=e;
-	       this.books=newBook;
-    }
+	/**
+	 * addBook(Book) : void
+	 * Creates a new array which is a copy of this.books, but, whose length is
+	 * this.books.length + 1, to be able to add the new book.
+	 *
+	 * this.books is then replaced by the new array.
+	 *
+	 * @param book
+	 *          A new book object to add to the books list
+	 */
+	public void addBook(Book e){
+		Book[]newBook=Book[books.length+1];
+		for(int i=0;i<books.length;i++){
+			newBook[i]=books[i];
+		}
+		newBook[books.length]=e;
+		this.books=newBook;
+	}
 
-    /**
-     * addMember(Member) : void
-     * Creates a new array which is a copy of this.members, but, whose length is
-     * this.members.length + 1, to be able to add the new member.
-     *
-     * this.members is then replaced by the new array.
-     *
-     * @param member
-     *          A new member object to add to the members list
-     */
-     public void addMember(Member e){
+	/**
+	 * addMember(Member) : void
+	 * Creates a new array which is a copy of this.members, but, whose length is
+	 * this.members.length + 1, to be able to add the new member.
+	 *
+	 * this.members is then replaced by the new array.
+	 *
+	 * @param member
+	 *          A new member object to add to the members list
+	 */
+	public void addMember(Member e){
 		Member[]newMember=Member[Members.length+1];
 		for(int i=0;i<Members.length;i++){
 			newMember[i]=Members[i];
