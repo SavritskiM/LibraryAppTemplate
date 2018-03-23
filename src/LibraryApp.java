8608ff9e28951f3ae4c95a7135ebb1d2d0332794
michael <michaelsavr@gmail.com>
2018-03-23 09:32:35 -0400

    .

--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -3,6 +3,18 @@ public class LibraryApp {
     private static Library library = new Library();
 
     public static void main(String[] args) {
-        System.out.println(library);
+    Book book1 = new Book();
+    book1.title = "To kill a Mockingbird ";
+    book1.author = "tom jeff";
+    book1.pages =new String[] {"1", "2", "4", "5"};
+
+    Book book2 = new Book();
+    book2.title = "Jeff";
+    book2.author = "Jeff jeff";
+    book2.pages =new String[] {"1", "2", "4", "5"};
+    library.addBook(book1);
+    library.addBook(book2);
+
+    
     }
 }
