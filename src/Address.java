e7b4ee8eb114cbcb214f01f541c3a2ad3c754986
maysonho <36411187+maysonho@users.noreply.github.com>
2018-03-23 09:15:15 -0400

    Update Address.java

--- src/Address.java
+++ src/Address.java
@@ -6,6 +6,18 @@ public class Address {
     String city;
     String province;
     String postalCode;
+    /**
+     * toString() : void
+     * Overrides the default toString() method.
+     * This will format the address in the form of
+     *
+     * street
+     * city, province
+     * postalCode
+     *
+     * @return
+     *          String consisting of a formatted mailing address.
+     */
     public String toString() {
         return this.street + ", " + this.city + ", " + this.state + " " + this.postalCode;
     }
