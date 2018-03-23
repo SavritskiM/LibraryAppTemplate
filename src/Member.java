b5fcffec0cffa273d7a067bf913364ef2b54fcff
LLiu6 <36411010+LLiu6@users.noreply.github.com>
2018-03-23 09:19:29 -0400

    Update Member.java

--- src/Member.java
+++ src/Member.java
@@ -3,40 +3,33 @@
  */
 public class Member {
 
-    String name;
-    int birthyear;
-    Address address = new Address();
-    
-
-    /**
-     * getAge() : int
-     * Instance method to calculate the member's age.
-     * @return
-     *          returns the member's age as an int.
-     */
-    private int getAge(){
-     int age;
-     age = 2018 - birthyear;
-     return age;
-    }
+	String name;
+	int birthyear;
+	Address address;
 
 
+	/**
+	 * getAge() : int
+	 * Instance method to calculate the member's age.
+	 * @return
+	 *          returns the member's age as an int.
+	 */
+	private int getAge(){
+		int age;
+		age = 2018 - birthyear;
+		return age;
+	}
 
-
-    /**
-     * readBook(Book) : void
-     * Will "read" a book give to the member.
-     * "Reading" consists of printing out any appropriate message
-     * containing the title of the book.
-     *
-     * @param book
-     *          The book to read.
-     */
-    private void int readBook(book){
-     System.out.println("you are reading: " + book.title);
-        
-    }
-
-
-
+	/**
+	 * readBook(Book) : void
+	 * Will "read" a book give to the member.
+	 * "Reading" consists of printing out any appropriate message
+	 * containing the title of the book.
+	 *
+	 * @param book
+	 *          The book to read.
+	 */
+	private void readBook(Book e){
+		System.out.println("you are reading: " + e.title);
+	}
 }
