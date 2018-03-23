3eb69ce5e436d942233358a4b214e467e2a118ca
LLiu6 <36411010+LLiu6@users.noreply.github.com>
2018-03-23 09:44:36 -0400

    Update LibraryApp.java

--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -16,12 +16,12 @@ public class LibraryApp {
 		member1.birthyear=2001;
 		library.addMember(member1);
 		
-		System.out.println(" the members in this library are: ");
+		System.out.println("The member(s) in this library are: ");
 		
 		for(int i=0;i<library.members.length;i++){
 			System.out.println(library.members[i].name);
 		}
-		
+		System.out.println();
 		// Print Books
 		library.books = new Book[0];
 		Book book1 = new Book();
@@ -35,10 +35,11 @@ public class LibraryApp {
 		book2.pages =new String[] {"1", "2", "4", "5"};
 		library.addBook(book1);
 		library.addBook(book2);
-		System.out.println(" the books in this library are: ");
+		System.out.println("The books in this library are: ");
 		for(int i=0;i<library.books.length;i++){
-			System.out.println(library.books[i].title);
+			System.out.println("-"+library.books[i].title);
 		}
+		System.out.println();
 		
 		// Print Address
 		Address location=new Address();
@@ -47,7 +48,7 @@ public class LibraryApp {
 		location.province="ON";
 		location.street="Leslie";
 		library.location=location;
-		System.out.println(" our mailing address is: ");
+		System.out.println("Our mailing address is: ");
 		System.out.println(library.location.toString());
 
 
