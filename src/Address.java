5edf116e6a82e974b1f00c6b89adf9c048f48090
maysonho <36411187+maysonho@users.noreply.github.com>
2018-03-23 09:09:29 -0400

    Update Address.java

--- src/Address.java
+++ src/Address.java
@@ -4,7 +4,7 @@
 public class Address {
     String street;
     String city;
-    String state;
+    String province;
     String postalCode;
     public String toString() {
         return this.street + ", " + this.city + ", " + this.state + " " + this.postalCode;
