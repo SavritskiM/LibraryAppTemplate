c6fd81702b920bfbef4d896ec14a29c734b26550
Joshua <siyuan.zhang18@ycdsbk12.ca>
2018-03-23 08:49:25 -0400

    Update Address.java

--- src/Address.java
+++ src/Address.java
@@ -17,8 +17,11 @@ public class Address {
      *          String consisting of a formatted mailing address.
      */
 
-
-
-
-
+class Address {
+    String street;
+    String city;
+    String state;
+    String postalCode;
+    public String toString() {
+        return this.street + ", " + this.city + ", " + this.state + " " + this.postalCode;
 }
