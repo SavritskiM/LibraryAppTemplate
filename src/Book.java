a3b2c4c041289db0d2838dd2d09074bdc2cacba6
LLiu6 <36411010+LLiu6@users.noreply.github.com>
2018-03-23 09:06:11 -0400

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -2,44 +2,40 @@
  * Book class
  */
 public class Book {
- String title;
-  String author;
-  String [] Pages;
- 
+	String title;
+	String author;
+	String [] pages;
+	/**
+	 * censorPage(int) : void
+	 * Will replace the contents of any given pageNumber with a hyphen "-", thereby
+	 * 'censoring' its contents from being viewed.
+	 *
+	 * @param pageNumber
+	 *          The index of the page to be 'censored'
+	 *
+	 */
+	public void censorPage(int index){
+		String censor="";
+		for(int i=0;i<pages[index].length();i++){
+			censor+="-";
+		}
+		this.pages[index]=censor;
+	}
+	/**
+	 * toString() : String
+	 * Returns all this.pages values as one long string.
+	 *
+	 * @return
+	 *          String of all page contents together.
+	 *          E.g. if this.pages = { "a", "b", "c" };
+	 *          the method returns "abc".
+	 */
 
-    /**
-     * censorPage(int) : void
-     * Will replace the contents of any given pageNumber with a hyphen "-", thereby
-     * 'censoring' its contents from being viewed.
-     *
-     * @param pageNumber
-     *          The index of the page to be 'censored'
-     *
-     */
-    
- 
-//  public void censorPage(int){
-//    return "";
-//  }
-
-
-
-
-
-
-
-    /**
-     * toString() : String
-     * Returns all this.pages values as one long string.
-     *
-     * @return
-     *          String of all page contents together.
-     *          E.g. if this.pages = { "a", "b", "c" };
-     *          the method returns "abc".
-     */
-  
-  public String toString () {
-    return "";
-  }
+	public String toString () {
+		String all="";
+		for(int i=0;i<pages.length;i++){
+			all+=pages[i];
+		}
+		return all;
+	}
 }
-
